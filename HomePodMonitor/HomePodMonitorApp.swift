af0addccbd38c2063eab48f41e3d11aab76eb6fa
chenyungui <linxitj2003@icloud.com>
2026-03-06 12:49:16 +0800

    Initial Commit

---
+++ HomePodMonitor/HomePodMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePodMonitorApp.swift
+//  HomePodMonitor
+//
+//  Created by chenyungui on 2026/3/6.
+//
+
+import SwiftUI
+
+@main
+struct HomePodMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
